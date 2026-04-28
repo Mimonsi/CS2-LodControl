47cc28dcf1141b9a7ad9910ad1ff8f840729ea84
Mimonsi <konstantin-schwarze@outlook.de>
2026-04-28 23:38:11 +0200

    WIP

--- Mod.cs
+++ Mod.cs
@@ -1,9 +1,9 @@
+using Colossal.IO.AssetDatabase;
 using Colossal.Logging;
 using Game;
 using Game.Input;
 using Game.Modding;
 using Game.SceneFlow;
-using Colossal.IO.AssetDatabase;
 using UnityEngine.InputSystem;
 
 namespace LodControl
@@ -28,9 +28,17 @@ namespace LodControl
 
             m_Setting.RegisterKeyBindings();
 
-            var toggleAction = m_Setting.GetAction(Setting.kToggleDisableLodAction);
-            toggleAction.shouldBeEnabled = true;
-            toggleAction.onInteraction += OnToggleDisableLod;
+            RegisterAction(Setting.kToggleDisableLodAction, OnToggleDisableLod);
+            RegisterAction(Setting.kPreset1Action, (_, phase) => OnApplyPreset(phase, m_Setting.Preset1Value, "Preset 1"));
+            RegisterAction(Setting.kPreset2Action, (_, phase) => OnApplyPreset(phase, m_Setting.Preset2Value, "Preset 2"));
+            RegisterAction(Setting.kPreset3Action, (_, phase) => OnApplyPreset(phase, m_Setting.Preset3Value, "Preset 3"));
+        }
+
+        private void RegisterAction(string actionName, System.Action<ProxyAction, InputActionPhase> handler)
+        {
+            var action = m_Setting.GetAction(actionName);
+            action.shouldBeEnabled = true;
+            action.onInteraction += handler;
         }
 
         private void OnToggleDisableLod(ProxyAction action, InputActionPhase phase)
@@ -38,6 +46,13 @@ namespace LodControl
             if (phase != InputActionPhase.Performed) return;
             m_Setting.DisableLodModels = !m_Setting.DisableLodModels;
             log.Info($"DisableLodModels toggled to {m_Setting.DisableLodModels}");
+        }
+
+        private void OnApplyPreset(InputActionPhase phase, float lodValue, string presetName)
+        {
+            if (phase != InputActionPhase.Performed) return;
+            m_Setting.ApplyLodValue(lodValue);
+            log.Info($"{presetName} applied with LevelOfDetail {lodValue}");
         }
 
         public void OnDispose()
--- Setting.cs
+++ Setting.cs
@@ -2,29 +2,43 @@ using Colossal;
 using Colossal.IO.AssetDatabase;
 using Game.Input;
 using Game.Modding;
+using Game.Rendering;
 using Game.Settings;
 using System.Collections.Generic;
-using Game.Rendering;
 using Unity.Entities;
 
 namespace LodControl
 {
     [FileLocation("ModsSettings/" + nameof(LodControl) + "/" + nameof(LodControl))]
-    [SettingsUIGroupOrder(kGroup)]
+    [SettingsUITabOrder(kSection, kPresetsSection)]
+    [SettingsUIGroupOrder(kGroup, kPreset1Group, kPreset2Group, kPreset3Group)]
+    [SettingsUIShowGroupName(kPreset1Group, kPreset2Group, kPreset3Group)]
     public class Setting : ModSetting
     {
         public const string kSection = "Main";
-        public const string kGroup = "Group";
+        public const string kPresetsSection = "Presets";
+        public const string kGroup = "MainGroup";
+        public const string kPreset1Group = "Preset1Group";
+        public const string kPreset2Group = "Preset2Group";
+        public const string kPreset3Group = "Preset3Group";
+        public const string kVanillaPresetButtonGroup = "VanillaPresetButtonGroup";
         public const string kToggleDisableLodAction = nameof(ToggleDisableLodModelsBinding);
+        public const string kPreset1Action = nameof(Preset1Binding);
+        public const string kPreset2Action = nameof(Preset2Binding);
+        public const string kPreset3Action = nameof(Preset3Binding);
+
         private readonly RenderingSystem _renderingSystem;
+        private float _levelOfDetail;
+        private float _preset1Value = 1f;
+        private float _preset2Value = 1f;
+        private float _preset3Value = 1f;
 
         public Setting(IMod mod) : base(mod)
         {
             _renderingSystem = World.DefaultGameObjectInjectionWorld?.GetExistingSystemManaged<RenderingSystem>();
         }
 
-        private float _levelOfDetail;
-
+        [SettingsUISection(kSection, kGroup)]
         [SettingsUISlider(min = 0f, max = 1000f, step = 10f, unit = "percentage", scalarMultiplier = 100f)]
         public float LevelOfDetail
         {
@@ -36,14 +50,110 @@ namespace LodControl
             }
         }
 
+        [SettingsUISection(kSection, kGroup)]
+        [SettingsUIButton]
+        [SettingsUIButtonGroup(kVanillaPresetButtonGroup)]
+        public bool VeryLowPreset
+        {
+            get => false;
+            set
+            {
+                if (value)
+                    ApplyLodValue(0.25f);
+            }
+        }
+
+        [SettingsUISection(kSection, kGroup)]
+        [SettingsUIButton]
+        [SettingsUIButtonGroup(kVanillaPresetButtonGroup)]
+        public bool LowPreset
+        {
+            get => false;
+            set
+            {
+                if (value)
+                    ApplyLodValue(0.35f);
+            }
+        }
+
+        [SettingsUISection(kSection, kGroup)]
+        [SettingsUIButton]
+        [SettingsUIButtonGroup(kVanillaPresetButtonGroup)]
+        public bool MediumPreset
+        {
+            get => false;
+            set
+            {
+                if (value)
+                    ApplyLodValue(0.5f);
+            }
+        }
+
+        [SettingsUISection(kSection, kGroup)]
+        [SettingsUIButton]
+        [SettingsUIButtonGroup(kVanillaPresetButtonGroup)]
+        public bool HighPreset
+        {
+            get => false;
+            set
+            {
+                if (value)
+                    ApplyLodValue(0.7f);
+            }
+        }
+
+        [SettingsUISection(kSection, kGroup)]
         public bool DisableLodModels
         {
             get => _renderingSystem.disableLodModels;
             set => _renderingSystem.disableLodModels = value;
         }
 
+        [SettingsUISection(kSection, kGroup)]
         [SettingsUIKeyboardBinding(BindingKeyboard.None, kToggleDisableLodAction)]
         public ProxyBinding ToggleDisableLodModelsBinding { get; set; }
+
+        [SettingsUISection(kPresetsSection, kPreset1Group)]
+        [SettingsUISlider(min = 0f, max = 1000f, step = 10f, unit = "percentage", scalarMultiplier = 100f)]
+        public float Preset1Value
+        {
+            get => _preset1Value;
+            set => _preset1Value = value;
+        }
+
+        [SettingsUISection(kPresetsSection, kPreset1Group)]
+        [SettingsUIKeyboardBinding(BindingKeyboard.None, kPreset1Action)]
+        public ProxyBinding Preset1Binding { get; set; }
+
+        [SettingsUISection(kPresetsSection, kPreset2Group)]
+        [SettingsUISlider(min = 0f, max = 1000f, step = 10f, unit = "percentage", scalarMultiplier = 100f)]
+        public float Preset2Value
+        {
+            get => _preset2Value;
+            set => _preset2Value = value;
+        }
+
+        [SettingsUISection(kPresetsSection, kPreset2Group)]
+        [SettingsUIKeyboardBinding(BindingKeyboard.None, kPreset2Action)]
+        public ProxyBinding Preset2Binding { get; set; }
+
+        [SettingsUISection(kPresetsSection, kPreset3Group)]
+        [SettingsUISlider(min = 0f, max = 1000f, step = 10f, unit = "percentage", scalarMultiplier = 100f)]
+        public float Preset3Value
+        {
+            get => _preset3Value;
+            set => _preset3Value = value;
+        }
+
+        [SettingsUISection(kPresetsSection, kPreset3Group)]
+        [SettingsUIKeyboardBinding(BindingKeyboard.None, kPreset3Action)]
+        public ProxyBinding Preset3Binding { get; set; }
+
+        public void ApplyLodValue(float value)
+        {
+            DisableLodModels = false;
+            LevelOfDetail = value;
+        }
 
         private void ChangeLodDistance(float value)
         {
@@ -55,6 +165,11 @@ namespace LodControl
 
         public override void SetDefaults()
         {
+            LevelOfDetail = 1f;
+            DisableLodModels = false;
+            Preset1Value = 1f;
+            Preset2Value = 1f;
+            Preset3Value = 1f;
         }
     }
 
@@ -74,24 +189,73 @@ namespace LodControl
             {
                 { m_Setting.GetSettingsLocaleID(), "level of Detail Control" },
                 { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kGroup), "Group" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kPresetsSection), "Presets" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kGroup), "Main" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kPreset1Group), "Preset 1" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kPreset2Group), "Preset 2" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kPreset3Group), "Preset 3" },
+
                 { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LevelOfDetail)), "Level of Detail" },
                 {
                     m_Setting.GetOptionDescLocaleID(nameof(Setting.LevelOfDetail)),
                     "Change distance where Level of Detail applies. Base game has values from 0-100%, this allows more freedom."
                 },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.VeryLowPreset)), "Very Low" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LowPreset)), "Low" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.MediumPreset)), "Medium" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.HighPreset)), "High" },
+
                 { m_Setting.GetOptionLabelLocaleID(nameof(Setting.DisableLodModels)), "Disable LOD Models" },
                 {
                     m_Setting.GetOptionDescLocaleID(nameof(Setting.DisableLodModels)),
                     "Completely disable LOD Models. This renders everything at maximum quality. Not suitable for gameplay, but useful for screenshots"
                 },
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ToggleDisableLodModelsBinding)), "Toggle Disable LOD Models" },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ToggleDisableLodModelsBinding)), "Hotkey" },
                 {
                     m_Setting.GetOptionDescLocaleID(nameof(Setting.ToggleDisableLodModelsBinding)),
                     "Hotkey to toggle the 'Disable LOD Models' setting on or off."
                 },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Preset1Value)), "Value" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Preset1Value)),
+                    "Value applied when Preset 1 is triggered."
+                },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Preset1Binding)), "Hotkey" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Preset1Binding)),
+                    "Hotkey that applies Preset 1."
+                },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Preset2Value)), "Value" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Preset2Value)),
+                    "Value applied when Preset 2 is triggered."
+                },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Preset2Binding)), "Hotkey" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Preset2Binding)),
+                    "Hotkey that applies Preset 2."
+                },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Preset3Value)), "Value" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Preset3Value)),
+                    "Value applied when Preset 3 is triggered."
+                },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Preset3Binding)), "Hotkey" },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Preset3Binding)),
+                    "Hotkey that applies Preset 3."
+                },
+
                 { m_Setting.GetBindingMapLocaleID(), "LOD Control" },
                 { m_Setting.GetBindingKeyLocaleID(Setting.kToggleDisableLodAction), "Toggle Disable LOD Models" },
+                { m_Setting.GetBindingKeyLocaleID(Setting.kPreset1Action), "Preset 1" },
+                { m_Setting.GetBindingKeyLocaleID(Setting.kPreset2Action), "Preset 2" },
+                { m_Setting.GetBindingKeyLocaleID(Setting.kPreset3Action), "Preset 3" },
             };
         }
 
